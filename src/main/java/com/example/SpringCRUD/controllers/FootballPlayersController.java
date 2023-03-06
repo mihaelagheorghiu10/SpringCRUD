97414bb57c0f9f618b8e11e61607150578d930be
jaume vidal <jaumevibu@gmail.com>
2023-03-06 12:50:20 +0100

    Add getById to FootballPlayersController
    Add getById method to FootballPlayersController class

--- src/main/java/com/example/SpringCRUD/controllers/FootballPlayersController.java
+++ src/main/java/com/example/SpringCRUD/controllers/FootballPlayersController.java
@@ -2,11 +2,13 @@ package com.example.SpringCRUD.controllers;
 
 import com.example.SpringCRUD.models.FootballPlayer;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/players")
@@ -20,8 +22,25 @@ public class FootballPlayersController {
     ) );
     @GetMapping
     public List<FootballPlayer> getAllPlayers(){
-        return playerList;
+        return this.playerList;
     }
+
+    @GetMapping("{id}")
+    public FootballPlayer getById(@PathVariable UUID id){
+//        for(FootballPlayer player:this.playerList){
+//            if(player.getId().equals(id)){
+//                return player;
+//            }
+//        }
+//        return null;
+        return this.playerList.stream()
+                .filter(
+                        player->player.getId()
+                                .equals(id))
+                .findFirst()
+                .get();
+    }
+
 
 
 
